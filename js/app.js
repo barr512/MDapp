9766629d689a0beff648989a486c58d81454b734
barr512 <brentshortusda@gmail.com>
2026-07-06 15:15:31 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+import { getInputs } from "./modules/inputs.js";
+import { generatePlanOptions } from "./modules/planner.js";
+import { renderSummary, renderOptions, selectPlan } from "./modules/renderer.js";
+
+const generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click", () => {
+  const input = getInputs();
+  const plans = generatePlanOptions(input);
+
+  renderSummary(input, plans.targetDispensers, plans.totalTrees);
+
+  renderOptions(plans.options, input, plans.targetDispensers, selectedPlan => {
+    selectPlan(selectedPlan, input, plans.targetDispensers);
+  });
+
+  selectPlan(plans.options[0], input, plans.targetDispensers);
+});
